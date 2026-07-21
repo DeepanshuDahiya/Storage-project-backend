3df3068d2d2987076d8c6941f49d3d3cd3ebbec2
DeepanshuDahiya <deepanshu31232@gmail.com>
2026-07-21 15:07:08 +0530

    use valid sender address in mail transporter

--- src/Services/email.service.js
+++ src/Services/email.service.js
@@ -3,7 +3,7 @@ import transporter from "../Config/nodemailer.js";
 export async function sendMail({ to, subject, html, text = "" }) {
   try {
     return transporter.sendMail({
-      from: `"Pro Store" <${process.env.MAIL_USER}>`,
+      from: `"Pro Store" <${process.env.MAIL_FROM}>`,
       to,
       subject,
       text,
